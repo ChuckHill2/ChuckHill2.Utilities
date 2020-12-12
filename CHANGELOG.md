bf322cd618f885f33d9648cb139ed6f25da70a7d
Chuck Hill <chuckhill2@gmail.com>
2020-12-12 01:56:55 -0800

    Changed target from .Net 4.5.1 to 4.5 + added doxygen and nuget package builder.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Change Log
+
+## 1.0.0
+* Initial Release
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Fixed
--- Source/ChuckHill2.Utilities/Properties/AssemblyInfo.cs
+++ Source/ChuckHill2.Utilities/Properties/AssemblyInfo.cs
@@ -1,36 +1,11 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Reflection;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ChuckHill2.Utilities")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("C# WinForms Utility Library")]
+[assembly: AssemblyDescription(@"C# WinForms Utility Library
+
+* Color handling components
+* Color palette winforms designer editor
+* Enum flags winforms designer editor
+* Gradient common controls")]
+
 [assembly: AssemblyProduct("ChuckHill2.Utilities")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44cf5926-3002-4c81-b44f-9975ec7d18ac")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
